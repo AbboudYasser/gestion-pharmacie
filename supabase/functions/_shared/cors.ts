749d545d4a7d36250ddbab80c21e98d8d346a1f2
ABBOUD_YASSER <minouminou07841@gmail.com>
2025-09-27 01:39:48 +0100

    fix: cors.ts

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,15 +1,16 @@
+// supabase/functions/_shared/cors.ts
 import { serve } from 'https://deno.land/std@0.168.0/http/server.ts'
 
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Origin': 'https://abboudyasser.github.io',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
   'Access-Control-Allow-Methods': 'POST, OPTIONS',
+  'Access-Control-Allow-Credentials': 'true' // إضافة هذا الرأس
 }
 
-export function handleOptions(req: Request) {
+export function handleOptions(req: Request ) {
   if (req.method === 'OPTIONS') {
-    return new Response('ok', { headers: corsHeaders })
+    return new Response('ok', { headers: corsHeaders, status: 200 }) // التأكد من حالة 200 OK
   }
   return null;
 }
-
